4c6a56ffae54d6211a19a3d7cfd069a69b43ff85
Felipe Menezes <lipe.menezes@live.com>
2018-06-17 02:49:21 -0300

    finish logic of video information api

--- app/api/videoInformationAPI.js
+++ app/api/videoInformationAPI.js
@@ -1,4 +1,5 @@
 const youtubeAPI = require('../api/youtubeAPI');
+const moment = require('moment');
 
 function getVideosWithValidDuration( {videos, biggestAvailableTime} ) {
 	let biggestAvailableTimeInSeconds = biggestAvailableTime * 60;
@@ -9,7 +10,7 @@ function getMostUsedWordsFromObject( {usedWords} ) {
 	let sortedKeys = Object.keys(usedWords).sort((a, b) => usedWords[a] - usedWords[b]);
 
 	let mostUsedWords = sortedKeys.slice(sortedKeys.length - 5, sortedKeys.length).map( (key) => {
-		return {key: key, timesUsed: usedWords[key]}
+		return {word: key, timesUsed: usedWords[key]}
 	});
 
 	return mostUsedWords;
@@ -19,9 +20,9 @@ function addUsedWordsToObject( {string, allUsedWords} ) {
 	let formattedString = string.replace(/[^a-zA-Z ]/g, "");
 	formattedString.split(' ').forEach(word => {
 		let formattedWord = word.toLowerCase();
-		if (allUsedWords.hasOwnProperty(formattedWord))
+		if (word.length && allUsedWords.hasOwnProperty(formattedWord))
 			allUsedWords[formattedWord] += 1
-		else
+		else if (word.length)
 			allUsedWords[formattedWord] = 1
 	});
 }
@@ -39,20 +40,40 @@ function getMostUsedWords( {videos} ) {
 	return mostUsedWords;
 }
 
-function getTimeRequiredToWatchAllVideos( {videos, availableTimeForDaysOfTheWeek} ) {
-	var dayOfTheWeek = 0;
+function getTimeScheduleToWatchAllVideos( {videos, availableTimeForDaysOfTheWeek} ) {
+	let dayOfTheWeek = moment().day();
+	
+	const availableTimeForDaysOfTheWeekInSeconds = availableTimeForDaysOfTheWeek.map(v => v * 60);
 
-	let videosToWatch = videos;
-	let 
+	let videosToWatch = videos.map(video => video);
+	let videosForDays = [];
 
 	while (dayOfTheWeek != null) {
+		let canAddVideo = true;
+		let availableTimeForDay = availableTimeForDaysOfTheWeekInSeconds[dayOfTheWeek];
+		let videosForDay = [];
+
+		while (canAddVideo) {
+			if (videosToWatch[0].duration < availableTimeForDay) {
+				videosForDay.push(videosToWatch[0]);
+				availableTimeForDay -= videosToWatch[0].duration;
+				videosToWatch.shift();
+				if (videosToWatch.length == 0) {
+					dayOfTheWeek = null;
+					canAddVideo = false;
+				}
+			} else {
+				canAddVideo = false;
+			}
+		}
+
+		videosForDays.push(videosForDay);
 		
-		
-		dayOfTheWeek += 1;
-		if (dayOfTheWeek > 7) dayOfTheWeek = 0
-		if (videosToWatch.length == 0) dayOfTheWeek = null;
+		if (dayOfTheWeek != null) dayOfTheWeek += 1;
+		if (dayOfTheWeek != null && dayOfTheWeek >= 7) dayOfTheWeek = 0
 	}
 
+	return videosForDays;
 }
 
 function getVideosData( {availableTimeForDaysOfTheWeek, maxResults, query} ) {
@@ -69,25 +90,36 @@ function getVideosData( {availableTimeForDaysOfTheWeek, maxResults, query} ) {
 				return {validVideos, mostUsedWords}
 			})
 			.then( ({validVideos, mostUsedWords}) => {
-				let timeRequiredToWatchAllVideos = getTimeRequiredToWatchAllVideos({
+				let timeScheduleToWatchAllVideos = getTimeScheduleToWatchAllVideos({
 					videos: validVideos, 
 					availableTimeForDaysOfTheWeek
 				});
-				resolve( {} );
+
+				resolve({
+					timeScheduleToWatchAllVideos, 
+					videos: validVideos, 
+					words: mostUsedWords
+				});
 			})
 			.catch(err => reject('err ', err));
 	});
 
 }
 
+module.exports = function (maxResults, query, availableTimeForDaysOfTheWeek) {
+	return getVideosData( {maxResults, query, availableTimeForDaysOfTheWeek} );
+}
 
+// const maxResults = 200;
+// const query = 'python';
+// const availableTimeForDaysOfTheWeek = [50, 30, 120, 190, 250, 0, 15];
 
-let availableTimeForDaysOfTheWeek = [20, 30, 40, 150, 180, 0, 10];
-let query = 'python';
-let maxResults = 50;
-
-
-getVideosData( {availableTimeForDaysOfTheWeek, maxResults, query} )
-	.then(data => console.log('finish\n', data))
-	.catch(err => console.log('err \n', err))
-
+// getVideosData( {maxResults, query, availableTimeForDaysOfTheWeek} )
+// 	.then(data => {
+// 		data.videos.length
+// 		var i = 0;
+// 		data.timeScheduleToWatchAllVideos.forEach( (item) => {
+// 			item.forEach(it => i += 1)
+// 		})
+// 		console.log(i)
+// 	})
